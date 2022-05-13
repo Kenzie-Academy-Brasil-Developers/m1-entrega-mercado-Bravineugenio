99a8fb3c78257974ff02dba9cb0bebc4cccded4a
Bravineugenio <101228018+Bravineugenio@users.noreply.github.com>
2022-05-12 23:36:46 -0300

    Primeiro commit

--- products.js
+++ products.js
@@ -1,3 +1,52 @@
+const frutas = document.querySelector(".fruits");
+const drinks = document.querySelector(".drinks");
+const higyen = document.querySelector(".hygiene");
+//Mapeando os elementos pela classe
+products.map((elemento) => {
+  const listarProdutos = criarLista(elemento);
+
+  if (elemento.category == "Frutas"){ 
+    return frutas.appendChild(listarProdutos);
+  } if (elemento.category == "Bebidas"){ 
+    return drinks.appendChild(listarProdutos);
+  } if (elemento.category == "Higiene"){ 
+    return higyen.appendChild(listarProdutos);
+  }
+});
+//criando a lista dos produtos
+function criarLista(item) {
+  const productCard = document.createElement("li");
+  const productImg = document.createElement("img");
+  const productMain = document.createElement("main");
+  const productTitle = document.createElement("h1");
+  const productCategory = document.createElement("h5");
+  const productPrice = document.createElement("strong");
+
+  productCard.appendChild(productImg);
+  productCard.appendChild(productMain);
+  productMain.appendChild(productTitle);
+  productMain.appendChild(productCategory);
+  productMain.appendChild(productPrice);
+
+
+  productCard.classList = "product";
+  productImg.classList = "product-img";
+  productMain.classList = "product-main";
+  productTitle.classList = "product-title";
+  productCategory.classList = "product-category";
+  productPrice.classList = "product-price";
+
+  productImg.src =
+  products[0].image;
+  products[0].image === undefined ? "./img/products/no-img.svg"
+  productTitle.innerHTML = products[0].title;
+  productCategory.innerHTML = products[0].category;
+  productPrice.innerHTML = products[0].price
+
+}
+
+
+
 const products = [
   {
     title: "Uva Crimson",
@@ -88,3 +137,5 @@ const products = [
     imageDescription: "",
   },
 ];
+
+criarLista(products)
